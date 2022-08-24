140644297cd4514b1e2626e5d61fcd744cf333ad
wshuts <shutswilliam@gmail.com>
2022-08-23 22:12:03 -0500

    scene composition: axes, sine_wave display 2 cycles of a unit sine wave

--- vswr_animation_bill.py
+++ vswr_animation_bill.py
@@ -4,7 +4,10 @@ ONE_MINUTE =60
 
 class VswrAnimation(Scene):
     def construct(self):
-        self.wait(ONE_MINUTE)
+        axes = Axes(x_range=[0,2*PI], y_range=[-1,1])
+        sine_wave = axes.plot(lambda t: 1*np.sin(2*t))
+        self.add(axes, sine_wave)
+        #self.wait(ONE_MINUTE)
 
 with tempconfig({"quality": "high_quality", "preview": True}):
     scene = VswrAnimation()

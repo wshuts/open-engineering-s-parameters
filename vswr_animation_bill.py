550da178021f74b859d5d806f8726427dbaed4c6
wshuts <shutswilliam@gmail.com>
2022-08-24 13:46:25 -0500

    sine "traveling" from right to left; 5 cycles; 600 animations/min ;

--- vswr_animation_bill.py
+++ vswr_animation_bill.py
@@ -1,5 +1,9 @@
 from manim import *
 
+ANIMATIONS = 600
+CYCLES = 5
+RUN_TIME = 0.1
+
 class VswrAnimation(Scene):
     def construct(self):
         axes = Axes(x_range=[0,2*PI], y_range=[-1,1])
@@ -10,9 +14,10 @@ class VswrAnimation(Scene):
 
         self.add(axes, sine_wave, phase_shift)
 
-        phase_shifts = np.linspace(0,2*PI,600)
+
+        phase_shifts = np.linspace(0,(2*PI)*CYCLES,ANIMATIONS)
         for angle in phase_shifts:
-            self.play(phase_shift.animate.set_value(angle), run_time=0.1)
+            self.play(phase_shift.animate.set_value(angle), run_time=RUN_TIME)
 
 with tempconfig({"quality": "high_quality", "preview": True}):
     scene = VswrAnimation()

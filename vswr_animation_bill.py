b36a3c06327363ba492bb919b3f2641c209067c8
wshuts <shutswilliam@gmail.com>
2022-08-25 00:07:58 -0500

    refactoring: sine_wave calls

--- vswr_animation_bill.py
+++ vswr_animation_bill.py
@@ -1,27 +1,27 @@
 from manim import *
 
-ANIMATIONS = 60
-CYCLES = 2
+ANIMATIONS = 10
+CYCLES = 0.03
 RUN_TIME = 0.1
 
 class VswrAnimation(Scene):
     def construct(self):
 
-        def sine_wave(t):
-            return 1*np.sin(2*t)
+        def sine_wave(t, phase_shift=0.0, peak=1.0): 
+            return peak*np.sin(2*t+phase_shift)
 
         axes = Axes(x_range=[0,2*PI], y_range=[-2.5,2.5])
 
         phase_shift = ValueTracker(0)
 
         sine_wave_forward = axes.plot(sine_wave)
-        sine_wave_forward.add_updater(lambda mob: mob.become(axes.plot(lambda t: 1*np.sin(2*t+phase_shift.get_value()))))
+        sine_wave_forward.add_updater(lambda mob: mob.become(axes.plot(lambda t: sine_wave(t, phase_shift.get_value()))))
 
-        sine_wave_reverse = axes.plot(lambda t: sine_wave(t))
-        sine_wave_reverse.add_updater(lambda mob: mob.become(axes.plot(lambda t: 0.7*np.sin(2*t-phase_shift.get_value()))))
+        sine_wave_reverse = axes.plot(sine_wave)
+        sine_wave_reverse.add_updater(lambda mob: mob.become(axes.plot(lambda t: sine_wave(t, -phase_shift.get_value(), 0.7))))
 
-        sine_wave_total = axes.plot(lambda t: sine_wave(t))
-        sine_wave_total.add_updater(lambda mob: mob.become(axes.plot(lambda t: 1*np.sin(2*t+phase_shift.get_value()) + 0.7*np.sin(2*t-phase_shift.get_value()))))
+        sine_wave_total = axes.plot(sine_wave)
+        sine_wave_total.add_updater(lambda mob: mob.become(axes.plot(lambda t: sine_wave(t, phase_shift.get_value()) + sine_wave(t, -phase_shift.get_value(), 0.7))))
         
         self.add(axes, sine_wave_forward, sine_wave_reverse, sine_wave_total, phase_shift)
 

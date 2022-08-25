bcbf754357801124741c32d223fd3c0e21dd419e
wshuts <shutswilliam@gmail.com>
2022-08-25 00:58:46 -0500

    refactoring completed: updated plot functions defined

--- vswr_animation_bill.py
+++ vswr_animation_bill.py
@@ -1,33 +1,40 @@
 from manim import *
 
-ANIMATIONS = 10
-CYCLES = 0.03
+ANIMATIONS = 600
+CYCLES = 5
 RUN_TIME = 0.1
 
 class VswrAnimation(Scene):
     def construct(self):
+        valueTracker = ValueTracker(0)
+        axes = Axes(x_range=[0,2*PI], y_range=[-2.5,2.5])
 
         def sine_wave(t, phase_shift=0.0, peak=1.0): 
             return peak*np.sin(2*t+phase_shift)
 
-        axes = Axes(x_range=[0,2*PI], y_range=[-2.5,2.5])
+        def forward(t):
+            return sine_wave(t, valueTracker.get_value())
 
-        phase_shift = ValueTracker(0)
+        def reverse(t):
+            return sine_wave(t, -valueTracker.get_value(), 0.7)
+
+        def total(t):
+            return sine_wave(t, valueTracker.get_value()) + sine_wave(t, -valueTracker.get_value(), 0.7)
 
         sine_wave_forward = axes.plot(sine_wave)
-        sine_wave_forward.add_updater(lambda mob: mob.become(axes.plot(lambda t: sine_wave(t, phase_shift.get_value()))))
+        sine_wave_forward.add_updater(lambda mob: mob.become(axes.plot(forward)))
 
         sine_wave_reverse = axes.plot(sine_wave)
-        sine_wave_reverse.add_updater(lambda mob: mob.become(axes.plot(lambda t: sine_wave(t, -phase_shift.get_value(), 0.7))))
+        sine_wave_reverse.add_updater(lambda mob: mob.become(axes.plot(reverse)))
 
         sine_wave_total = axes.plot(sine_wave)
-        sine_wave_total.add_updater(lambda mob: mob.become(axes.plot(lambda t: sine_wave(t, phase_shift.get_value()) + sine_wave(t, -phase_shift.get_value(), 0.7))))
+        sine_wave_total.add_updater(lambda mob: mob.become(axes.plot(total)))
         
-        self.add(axes, sine_wave_forward, sine_wave_reverse, sine_wave_total, phase_shift)
+        self.add(axes, sine_wave_forward, sine_wave_reverse, sine_wave_total, valueTracker)
 
         phase_shifts = np.linspace(0,(2*PI)*CYCLES,ANIMATIONS)
-        for angle in phase_shifts:
-            self.play(phase_shift.animate.set_value(angle), run_time=RUN_TIME)
+        for phase_shift in phase_shifts:
+            self.play(valueTracker.animate.set_value(phase_shift), run_time=RUN_TIME)
 
 with tempconfig({"quality": "high_quality", "preview": True}):
     scene = VswrAnimation()

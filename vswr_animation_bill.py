c5f425f16bf2b337672e9d7364a7dbd3064cc19c
wshuts <shutswilliam@gmail.com>
2022-08-24 21:24:40 -0500

    refactoring: def sine_wave(t)

--- vswr_animation_bill.py
+++ vswr_animation_bill.py
@@ -6,17 +6,21 @@ RUN_TIME = 0.1
 
 class VswrAnimation(Scene):
     def construct(self):
+
+        def sine_wave(t):
+            return 1*np.sin(2*t)
+
         axes = Axes(x_range=[0,2*PI], y_range=[-2.5,2.5])
 
         phase_shift = ValueTracker(0)
 
-        sine_wave_forward = axes.plot(lambda t: 1*np.sin(2*t))
+        sine_wave_forward = axes.plot(lambda t: sine_wave(t))
         sine_wave_forward.add_updater(lambda mob: mob.become(axes.plot(lambda t: 1*np.sin(2*t+phase_shift.get_value()))))
 
-        sine_wave_reverse = axes.plot(lambda t: 1*np.sin(2*t))
+        sine_wave_reverse = axes.plot(lambda t: sine_wave(t))
         sine_wave_reverse.add_updater(lambda mob: mob.become(axes.plot(lambda t: 0.7*np.sin(2*t-phase_shift.get_value()))))
 
-        sine_wave_total = axes.plot(lambda t: 1*np.sin(2*t))
+        sine_wave_total = axes.plot(lambda t: sine_wave(t))
         sine_wave_total.add_updater(lambda mob: mob.become(axes.plot(lambda t: 1*np.sin(2*t+phase_shift.get_value()) + 0.7*np.sin(2*t-phase_shift.get_value()))))
         
         self.add(axes, sine_wave_forward, sine_wave_reverse, sine_wave_total, phase_shift)

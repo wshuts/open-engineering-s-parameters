93b8fdf2cf068a547b14e98c7fb1662601b029ce
wshuts <shutswilliam@gmail.com>
2022-08-26 22:47:08 -0500

    cleanup of shift call, use of width property

--- text_overlay.py
+++ text_overlay.py
@@ -4,10 +4,10 @@ class TextOverlay(Scene):
     def construct(self):
         title_properties = Text("Interpreting Standing Waves", color=YELLOW)
         title_properties.scale(0.7)
-        title_properties.to_edge(UP).shift(LEFT*3.2+UP*0.1).shift(LEFT*0.5)
+        title_properties.to_edge(UP).shift(LEFT*3.7+UP*0.1)
 
         underline_properties = Line(LEFT, RIGHT, color=YELLOW)
-        underline_properties.set_width(1.1*title_properties.get_width())
+        underline_properties.width=1.1*title_properties.width
         underline_properties.next_to(title_properties, DOWN)
         underline_properties.shift(UP * 0.1)
 

7b5209869629ea22dfe50820c89afa75d31d128d
wshuts <shutswilliam@gmail.com>
2022-09-07 01:14:55 -0500

    title and underline

--- amplifier.py
+++ amplifier.py
@@ -2,19 +2,35 @@ from manim import *
 
 
 class Amplifier(MovingCameraScene):
+    title_properties = None
+    underline_properties = None
+
     def setup(self):
         MovingCameraScene.setup(self)
 
     # noinspection PyTypeChecker
     def construct(self):
         def create():
+            self.title_properties = Text("Example Networks", color=YELLOW)
+            self.underline_properties = Line(LEFT, RIGHT, color=YELLOW)
             return
 
         def stage():
+            self.title_properties.scale(1.2)
+            self.title_properties.to_edge(UP).shift(LEFT * 11 + DOWN * 5)
+            self.underline_properties.width = 1.1 * self.title_properties.width
+            self.underline_properties.next_to(self.title_properties, DOWN)
+            self.underline_properties.shift(UP * 0.1)
             return
 
         def animate():
+            self.frame = self.camera.frame
+            self.play(
+                self.frame.animate.set_width(26).move_to(2.2 * LEFT + 8 * DOWN))
+            self.play(FadeIn(self.title_properties, shift=LEFT), GrowFromCenter(self.underline_properties))
+            self.wait(3)
             return
+
 
         create()
         stage()

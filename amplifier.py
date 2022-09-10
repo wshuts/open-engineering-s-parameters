546afb2bd93ddcd950057c6d3aa73e0b8d392dcb
wshuts <shutswilliam@gmail.com>
2022-09-09 23:24:39 -0500

    def create_bulleted_list(tex_strings, under_mob): added parameters

--- amplifier.py
+++ amplifier.py
@@ -135,24 +135,24 @@ class Amplifier(MovingCameraScene):
             self.ax_amp.add(self.amplifier_s22)
             return
 
-        def create_bulleted_list():
-            self.networks = Tex("Antennas", "Dummy Loads", "Filters", "Attenuators", "Circulators", "Isolators",
-                                "Amplifiers")
-            for network in self.networks:
+        def create_bulleted_list(tex_strings, under_mob):
+            for tex_string in tex_strings:
                 dot = MathTex("\\cdot")
-                dot.scale(3).next_to(network[0], LEFT * 0.4, buff=0.4)
-                network.add_to_back(dot)
-            self.networks.arrange(DOWN, aligned_edge=LEFT, buff=0.3)
-            self.networks.scale(1.45)
-            self.networks.next_to(self.underline, DOWN)
-            self.networks.set_opacity(0.5)
+                dot.scale(3).next_to(tex_string[0], LEFT * 0.4, buff=0.4)
+                tex_string.add_to_back(dot)
+            tex_strings.arrange(DOWN, aligned_edge=LEFT, buff=0.3)
+            tex_strings.scale(1.45)
+            tex_strings.next_to(under_mob, DOWN)
+            tex_strings.set_opacity(0.5)
 
         def stage():
             self.title.scale(1.2)
             self.title.to_edge(UP)
             self.underline.width = 1.1 * self.title.width
             self.underline.next_to(self.title, DOWN)
-            create_bulleted_list()
+            self.networks = Tex("Antennas", "Dummy Loads", "Filters", "Attenuators", "Circulators", "Isolators",
+                                "Amplifiers")
+            create_bulleted_list(self.networks, self.underline)
             self.title_group = VGroup(self.title, self.underline, self.networks)
             self.title_group.shift(8 * LEFT + 3.6 * UP)
 
